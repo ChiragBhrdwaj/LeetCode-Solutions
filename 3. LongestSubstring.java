f427c1ee57ac0cab394b93fb651ad5f5ee8bfcc2
Chirag Bhardwaj <160613851+ChiragBhrdwaj@users.noreply.github.com>
2024-05-18 14:52:30 +0530

    Update 3. LongestSubstring.java

--- 3. LongestSubstring.java
+++ 3. LongestSubstring.java
@@ -1,10 +1,14 @@
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
+class Solution
+{
+    public int lengthOfLongestSubstring(String s)
+    {
         boolean[] ss = new boolean[128];
         int ans = 0;
-        for (int i = 0, j = 0; j < s.length(); ++j) {
+        for (int i = 0, j = 0; j < s.length(); ++j)
+        {
             char c = s.charAt(j);
-            while (ss[c]) {
+            while (ss[c])
+            {
                 ss[s.charAt(i++)] = false;
             }
             ss[c] = true;
