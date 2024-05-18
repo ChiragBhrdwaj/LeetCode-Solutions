6c491bfd222fff21b41143dbd31149c239427445
Chirag Bhardwaj <160613851+ChiragBhrdwaj@users.noreply.github.com>
2024-05-18 14:51:31 +0530

    Update 2. AddTwoNumbers.java

--- 2. AddTwoNumbers.java
+++ 2. AddTwoNumbers.java
@@ -1,12 +1,9 @@
-class Solution
-{
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2)
-  {
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode();
         int carry = 0;
         ListNode cur = dummy;
-        while (l1 != null || l2 != null || carry != 0)
-        {
+        while (l1 != null || l2 != null || carry != 0) {
             int s = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
             carry = s / 10;
             cur.next = new ListNode(s % 10);
