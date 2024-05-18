8c920c12c4bbd480532e69bc738609e43e6aafcb
Chirag Bhardwaj <160613851+ChiragBhrdwaj@users.noreply.github.com>
2024-05-18 14:50:39 +0530

    Update 1. TwoSum.java

--- 1. TwoSum.java
+++ 1. TwoSum.java
@@ -1,10 +1,14 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
+class Solution
+{
+    public int[] twoSum(int[] nums, int target)
+    {
         Map<Integer, Integer> m = new HashMap<>();
-        for (int i = 0;; ++i) {
+        for (int i = 0;; ++i)
+        {
             int x = nums[i];
             int y = target - x;
-            if (m.containsKey(y)) {
+            if (m.containsKey(y))
+            {
                 return new int[] {m.get(y), i};
             }
             m.put(x, i);
